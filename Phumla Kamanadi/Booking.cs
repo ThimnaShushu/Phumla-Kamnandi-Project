a9bd52cd0aaeba0a7c420055c03ee2133f2734af
ThimnaShushu <thimnashushu@gmail.com>
2024-09-18 01:42:33 +0200

    I added edits on the Branch, customer and booking classes

---
+++ Phumla Kamanadi/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Booking
+{
+    int BoooingID;
+    //string Guest;
+    int roomNum;
+    string status;
+    int totalCost;
+
+
+    public Booking(int BookingID, int roomNum, string status, int totalCost)
+	{
+        this.totalCost = totalCost;
+        this.BoooingID = BookingID;
+        this.roomNum = roomNum;
+        this.status = status;
+        
+        
+	}
+}
