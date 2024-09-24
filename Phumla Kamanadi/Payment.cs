a78742558e8ccb366a7085f2f89b88880728d964
EmanGit07 <emanafo24@outlook.com>
2024-09-24 17:41:54 +0200

    my files

---
+++ Phumla Kamanadi/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_INF2011S_Project
+{
+    internal class Payment
+    {
+    }
+}
