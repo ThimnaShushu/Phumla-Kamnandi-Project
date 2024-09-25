c258f9461fb2df721f7fd354a142d13cccfbc4b7
ntwananoblessing <129693227+ntwananoblessing@users.noreply.github.com>
2024-09-25 20:25:19 +0200

    Add files via upload

---
+++ Season.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace PhumlaKamnandiHotel
+{
+    public class Season
+    {
+        // Properties of the Season
+        public string SeasonName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal RatePerRoom { get; set; }
+
+        // Constructor
+        public Season(string seasonName, DateTime startDate, DateTime endDate, decimal ratePerRoom)
+        {
+            SeasonName = seasonName;
+            StartDate = startDate;
+            EndDate = endDate;
+            RatePerRoom = ratePerRoom;
+        }
+
+        // Method to check if a given date falls within the season
+        public bool IsDateInSeason(DateTime date)
+        {
+            return date >= StartDate && date <= EndDate;
+        }
+
+        // Static method to get the season rate for a given date
+        public static decimal GetRateForDate(DateTime date, Season[] seasons)
+        {
+            foreach (var season in seasons)
+            {
+                if (season.IsDateInSeason(date))
+                {
+                    return season.RatePerRoom;
+                }
+            }
+            throw new Exception("Date is not within any defined season.");
+        }
+    }
+}
